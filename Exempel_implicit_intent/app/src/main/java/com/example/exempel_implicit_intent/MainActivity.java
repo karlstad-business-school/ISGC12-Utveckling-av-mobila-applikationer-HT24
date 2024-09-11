cef9fea52027411a8357c5428f0892cbd988e42e
alexvest <78535319+alexvest@users.noreply.github.com>
2024-09-11 12:06:09 +0200

    Slutkod F4

--- Exempel_implicit_intent/app/src/main/java/com/example/exempel_implicit_intent/MainActivity.java
+++ Exempel_implicit_intent/app/src/main/java/com/example/exempel_implicit_intent/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.exempel_implicit_intent;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,45 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button btn_a = findViewById(R.id.button1);
+        Button btn_b = findViewById(R.id.button2);
+        Button btn_c = findViewById(R.id.button3);
+
+
+        btn_a.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:123456789"));
+
+                if(intent.resolveActivity(getPackageManager()) != null){
+                    startActivity(intent);
+                }
+            }
+        });
+
+
+        btn_b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("plain/text");
+                startActivity(Intent.createChooser(i, "Send your email in:"));
+            }
+        });
+
+
+        btn_c.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String ACTION = "abc.test.123";
+                Intent intent = new Intent();
+                intent.setAction(ACTION);
+
+                startActivity(intent);
+            }
+        });
+
     }
 }
